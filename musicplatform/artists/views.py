b5e40cbfb0c0783b67a7d8e4787c402f9407a8de
ssary <sarynasser1@gmail.com>
2022-11-05 23:40:48 +0200

    add add/post artist

--- musicplatform/artists/views.py
+++ musicplatform/artists/views.py
@@ -2,4 +2,31 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Artist
+from .pagination import CustomPagination
+from .serializers import ArtistSerializer
+
+class ArtistsView(APIView, CustomPagination):
+    def get(self, request):
+        artists = Artist.objects.all()
+        results = self.paginate_queryset(artists, request, view=self)
+        serializer = ArtistSerializer(results, many=True)
+        return Response(self.get_paginated_response(serializer.data))
+
+    def post(self, request):
+        serilaizer = ArtistSerializer(data=request.data)
+        if serilaizer.is_valid():
+            serilaizer.save()
+            return Response(serilaizer.data)
+        return Response(serilaizer.errors)
+
+class ArtistView(APIView):
+    def get(self, request, *args, **kwargs):
+        artistId = kwargs['id']
+        try:
+            artistFetched = Artist.objects.get(id=artistId)
+            serializer = ArtistSerializer(artistFetched)
+            return Response(serializer.data)
+        except IndexError:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
