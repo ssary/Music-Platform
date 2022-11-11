779ad8a205f8ff07e29847e55fe514d1b789b295
ssary <sarynasser1@gmail.com>
2022-11-11 11:32:07 +0200

    add Profile table

--- musicplatform/users/models.py
+++ musicplatform/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    bio = models.CharField(max_length=256, blank=True)
+
+    def __str__(self):
+        return str(self.user)
+
